091756b91ec2c826659cec0d5af788d9b9c7e6e6
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-12-22 20:39:31 -0300

    proyecto andando

---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Consessionario TusCar</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div><h2>dsmlk</h2></div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
